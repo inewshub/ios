ab34db3038365c3fff574f20ef1bdc2832bbbdef
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-08-27 03:52:31 -0500

    temp: screenshot FacilitiesView (LifeView) with navbar/tabbar chrome

--- inewshub/Pages/AllView.swift
+++ inewshub/Pages/AllView.swift
@@ -47,7 +47,7 @@ class DataManagerAll: ObservableObject {
 
 struct AllView: View {
     @StateObject private var dataManager = DataManagerAll()
-    @State private var selectedSection = 3
+    @State private var selectedSection = 2
     @Namespace private var underlineAnimation
     @State private var currentPage = 0
 
